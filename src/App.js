827f1f9e8808d978800f1900dbe07b3fb7b94bc5
gom <nobrain908@gmail.com>
2021-11-22 17:27:39 +0900

    s1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Average from "./Average";
+import EventPractice from "./EventPractice";
+import Info from "./Info";
+import IterationSample from "./IterationSample";
+
+
+function App() {
+  return (
+    <>
+  <Info />
+  <Average />
+  <EventPractice />
+  <IterationSample />
+  </>
+  )
+}
+
+export default App;
---
+++ src/IterationSample.js
@@ -0,0 +1,57 @@
+import React, { useState} from "react";
+
+const IterationSample = () => {
+    const [ names, setNames ] = useState([
+        {
+            id : 1, text : 'snowman'
+        },
+        {
+            id : 2, text : 'fireman'
+        },
+        {
+            id : 3, text : 'iceman'
+        },
+        {
+            id : 4, text : 'thunderstorm'
+        }
+    ]);
+    const [ inputText, setInputText] = useState('');
+    const [ nextId, setNextId ] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
+    const onRemove = id => {
+        const nextNames = names.filter( name => name.id !== id);
+        setNames(nextNames);
+    }
+    const namesList = names.map(name => (
+        <li key={name.id} onDoubleClick={ ()=> onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+    return (
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{namesList}</ul>
+        </>
+    )
+
+}
+export default IterationSample;
+
+
+
+
+
+
+
+
